0c4b1a9ba250cceefccf92fc49ec2fe5374feb7c
Katie Mangan <katie.mangan1@gmail.com>
2019-02-22 13:47:26 -0600

    move Header to component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                    React Components
+            </header>
+        );
+    }
+}
+
+export default Header;
