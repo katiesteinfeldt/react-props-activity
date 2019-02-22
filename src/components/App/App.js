58ca60242f718cd08a8c97e9d6fabd7e1f781f90
Katie Mangan <katie.mangan1@gmail.com>
2019-02-22 14:17:47 -0600

    run up function successfully

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,23 @@ import Header from '../Header/Header';
 
 class App extends Component {
   state = {
-    inputNumber:3,
+    currentTotal: '',
+    inputNumber: '',
   }
 
 
   handleInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({
+      inputNumber: Number(event.target.value),
+    })
   }
 
 
   handleButtonUpClick = () => {
     console.log('Button Up Clicked');
+    this.setState({
+      currentTotal: Number(this.state.inputNumber + this.state.currentTotal),
+    })
   }
 
   handleButtonDownClick = () => {
@@ -26,7 +32,7 @@ class App extends Component {
         <Header />
         <button onClick={this.handleButtonUpClick}>Up</button><input placeholder="Enter Number" type="number" onChange={this.handleInputChange}/><button onClick={this.handleButtonDownClick}>Down</button>
       
-        <h3>{this.state.inputNumber}</h3>
+        <h3>Current Total: {this.state.currentTotal}</h3>
 
 
       </div>
